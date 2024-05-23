73cb2e558d588fb4e3170cefb5ecee7a4bb37280
JL <johnlloydmedina.dev@gmail.com>
2024-05-23 13:58:19 +0800

    config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  resources :debtor
+  resources :loan_types
+
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  root to: 'dashboard#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+current_year = Time.now.year
+base_id_number = 1
+
+20.times do |i|
+  Debtor.create(
+    debtor_id: "#{current_year}#{format('%03d', base_id_number + i)}",
+    name: Faker::Name.name[0..49],
+    contact: Faker::PhoneNumber.cell_phone_in_e164[0..12],
+    email: Faker::Internet.email[0..19],
+    address: Faker::Address.full_address,
+    proof_of_billing: "proof-of-address.png",
+    identification: Faker::IDNumber.valid,
+    proof_of_identity: "proof-of-identity.jpg",
+    credit_score: rand(300..850),
+    is_active: rand(0..1),
+    remarks: Faker::Lorem.paragraph
+  )
+end
