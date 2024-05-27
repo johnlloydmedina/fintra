d08dcccc4d3fc6f090cd243efde8b3f279f708d8
JL <johnlloydmedina.dev@gmail.com>
2024-05-27 13:31:37 +0800

    improve pagination

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@ class MembersController < ApplicationController
   before_action :set_member, only: %i[show edit update destroy]
 
   def index
-    @members = Member.all
+    @members = Member.paginate(page: params[:page], per_page: 10)
   end
 
   def show
@@ -11,6 +11,7 @@ class MembersController < ApplicationController
 
   def new
     @member = Member.new
+    @member.member_id = generate_member_id
   end
 
   def edit
@@ -46,5 +47,18 @@ class MembersController < ApplicationController
 
     def member_params
       params.require(:member).permit(:first_name, :middle_name, :last_name, :complete_address, :contact_number, :email_address, :gender, :civil_status, :birthdate, :age, :profile_picture, :username, :password, :password_confirmation, :account_status)
+    end
+
+    def generate_member_id
+      year = Time.now.year
+      last_member = Member.where("member_id LIKE ?", "#{year}%").order(member_id: :desc).first
+      if last_member.present?
+        string_year = year.to_s
+        last_id_number = last_member.member_id.split(string_year).last.to_i
+        new_id_number = last_id_number + 1
+        member_id = "#{year}#{'%03d' % new_id_number}"
+      else
+        member_id = "#{year}001"
+      end
     end
 end
