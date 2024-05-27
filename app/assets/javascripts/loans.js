a6ebf61b0043a47676a60034716dcc0075e65285
JL <johnlloydmedina.dev@gmail.com>
2024-05-27 10:08:11 +0800

    Loan duration field event

--- app/assets/javascripts/loans.js
+++ app/assets/javascripts/loans.js
@@ -1,2 +1,9 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function() {
+  $('#loan_type_id').on('change', function() {
+    var loanDurations = $(this).data('loan-durations');
+    var selectedLoanTypeId = $(this).val();
+    var loanDuration = loanDurations[selectedLoanTypeId];
+
+    $('#loan_duration').val(loanDuration || '');
+  });
+});
