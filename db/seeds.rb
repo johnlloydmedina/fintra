26fa85eb324648da6c77a5c312d730329036ad20
JL <johnlloydmedina.dev@gmail.com>
2024-05-28 15:53:21 +0800

    delete uploads

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,30 @@
-require 'faker'
+# require 'faker'
 
-current_year = Time.now.year
-base_id_number = 1
+# current_year = Time.now.year
+# base_id_number = 1
 
-100.times do |i|
-  Member.create!(
-    member_id: "#{current_year}#{format('%03d', base_id_number + i)}",
-    first_name: Faker::Name.first_name,
-    middle_name: Faker::Name.middle_name,
-    last_name: Faker::Name.last_name,
-    complete_address: Faker::Address.full_address,
-    contact_number: Faker::PhoneNumber.phone_number,
-    email_address: Faker::Internet.email,
-    gender: ['Male', 'Female'].sample,
-    civil_status: ['Single', 'Married', 'Divorced', 'Widowed'].sample,
-    birthdate: Faker::Date.between(from: '1950-01-01', to: '2000-12-31'),
-    age: Faker::Number.between(from: 18, to: 70),
-    profile_picture: "proof-of-identity.jpg",
-    username: Faker::Internet.username,
-    password_digest: Faker::Internet.password(min_length: 8),
-    account_status: [0, 1].sample
-  )
-end
+# 100.times do |i|
+#   Member.create!(
+#     member_id: "#{current_year}#{format('%03d', base_id_number + i)}",
+#     first_name: Faker::Name.first_name,
+#     middle_name: Faker::Name.middle_name,
+#     last_name: Faker::Name.last_name,
+#     complete_address: Faker::Address.full_address,
+#     contact_number: Faker::PhoneNumber.phone_number,
+#     email_address: Faker::Internet.email,
+#     gender: ['Male', 'Female'].sample,
+#     civil_status: ['Single', 'Married', 'Divorced', 'Widowed'].sample,
+#     birthdate: Faker::Date.between(from: '1950-01-01', to: '2000-12-31'),
+#     age: Faker::Number.between(from: 18, to: 70),
+#     profile_picture: "cat-5.png",
+#     username: Faker::Internet.username,
+#     password_digest: Faker::Internet.password(min_length: 8),
+#     account_status: [0, 1].sample
+#   )
+# end
+
+LoanType.create!([
+  { loan_type_name: "Regular loan", description: "Standard loan", interest: 20, duration: "1 month", penalty: 5 },
+  { loan_type_name: "Fast cash", description: "Fast cash release", interest: 5, duration: "1 week", penalty: 5 },
+  { loan_type_name: "Annual", description: "Yearly ammortization", interest: 20, duration: "1 year", penalty: 5 }
+])
