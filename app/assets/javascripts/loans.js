3f39fe6e96e09365d0bdd7d90927ce828ade32c6
JL <johnlloydmedina.dev@gmail.com>
2024-05-29 15:36:35 +0800

    compute loan balance on loan type selection

--- app/assets/javascripts/loans.js
+++ app/assets/javascripts/loans.js
@@ -1,14 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  $('#loan_type_id').on('change', function() {
-    var loanDurations = $(this).data('loan-durations');
-    var selectedLoanTypeId = $(this).val();
-    var loanDuration = loanDurations[selectedLoanTypeId];
-
-    $('#loan_duration').val(loanDuration || '');
-  });
-});
-
-$(document).on('turbolinks:load', function() {
   $('a[data-remote=true]').on('ajax:success', function(event) {
     var detail = event.detail;
     var data = detail[0], status = detail[1], xhr = detail[2];
@@ -18,3 +8,32 @@ $(document).on('turbolinks:load', function() {
     $('#loanModal').modal('show');
   });
 });
+
+
+  $(document).on('turbolinks:load', function() {
+    var loanTypeSelect = $('#loan_type_id');
+    var loanAmountInput = $('#loan_amount');
+    var loanBalanceInput = $('#loan_balance');
+    
+    var loanDurations = loanTypeSelect.data('loan-durations');
+    var loanInterests = loanTypeSelect.data('loan-interests');
+
+    function updateLoanDetails() {
+      var selectedLoanTypeId = loanTypeSelect.val();
+      
+      var loanDuration = loanDurations[selectedLoanTypeId];
+      $('#loan_duration').val(loanDuration || '');
+
+      var loanAmount = parseFloat(loanAmountInput.val());
+      if (selectedLoanTypeId && !isNaN(loanAmount)) {
+        var interestRate = loanInterests[selectedLoanTypeId] || 0;
+        var loanBalance = loanAmount + (loanAmount * interestRate / 100);
+        loanBalanceInput.val(loanBalance.toFixed(2));
+      } else {
+        loanBalanceInput.val('');
+      }
+    }
+
+    loanTypeSelect.on('change', updateLoanDetails);
+    loanAmountInput.on('input', updateLoanDetails);
+  });
