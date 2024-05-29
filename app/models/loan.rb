e05cd60b95849fe6655778b96a0e85e85aae3a5e
JL <johnlloydmedina.dev@gmail.com>
2024-05-29 15:40:27 +0800

    add loan balance

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -5,7 +5,7 @@ class Loan < ApplicationRecord
 
   enum mode_of_payment: { over_the_counter: 0, bank_transfer: 1, salary_deduction: 2 }
 
-  validates :control_number, :member_id, :loan_type_id, :loan_amount, :loan_duration, :loan_status, presence: true
+  validates :control_number, :member_id, :loan_type_id, :loan_amount, :loan_balance, :loan_duration, :loan_status, presence: true
   validates :purpose, :remarks, length: { maximum: 500 }
   
   before_validation :set_loan_duration_based_on_type
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,15 +2,15 @@ class Payment < ApplicationRecord
   belongs_to :member
   belongs_to :loan
 
-  after_create :update_loan_amount
+  after_create :update_loan_balance
 
   private
 
-  def update_loan_amount
-    loan = Loan.find_by(member_id: self.member_id)
+  def update_loan_balance
+    loan = Loan.find_by(id: self.loan_id)
     if loan
-      new_amount = loan.loan_amount - self.payment_amount
-      loan.update(loan_amount: new_amount)
+      new_balance = loan.loan_balance - self.payment_amount
+      loan.update(loan_balance: new_balance)
     end
   end
 end
